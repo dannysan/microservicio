08f16fbb85a5b3ec96323d73935df4ec1b3a2b29
dannysan <dannysantqa@gmail.com>
2022-08-07 13:39:40 -0500

    Create Persona.cs

---
+++ ejercicio1/ejercicio1/Models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejercicio1.Models
+{
+    public partial class Persona
+    {
+        public int PersId { get; set; }
+        public string? PersNombre { get; set; }
+        public string? PersGenero { get; set; }
+        public int? PersEdad { get; set; }
+        public string? PersIdentificacion { get; set; }
+        public string? PersDireccion { get; set; }
+        public string? PersTelefono { get; set; }
+    }
+}
