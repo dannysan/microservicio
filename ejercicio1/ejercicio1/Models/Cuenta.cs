a65821aaa1422d9c2606701a0a9d51dfcc15d3be
dannysan <dannysantqa@gmail.com>
2022-08-07 13:39:52 -0500

    Create Cuenta.cs

---
+++ ejercicio1/ejercicio1/Models/Cuenta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejercicio1.Models
+{
+    public partial class Cuenta
+    {
+        public int CuenId { get; set; }
+        public int? ClieId { get; set; }
+        public string? CuenNumero { get; set; }
+        public string? CuenTipo { get; set; }
+        public double? CuenSaldoInicial { get; set; }
+        public bool? CuenEstado { get; set; }
+    }
+}
