b11ec9d7b1361eeb43dd294e776f8df83d33c619
dannysan <dannysantqa@gmail.com>
2022-08-07 13:39:49 -0500

    Create Movimiento.cs

---
+++ ejercicio1/ejercicio1/Models/Movimiento.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejercicio1.Models
+{
+    public partial class Movimiento
+    {
+        public int MoviId { get; set; }
+        public int? CuenId { get; set; }
+        public DateTime? MoviFecha { get; set; }
+        public double? MoviValor { get; set; }
+        public double? MoviSaldo { get; set; }
+    }
+
+
+    public partial class MovimientoCuenta    {
+
+        public string NumeroCuenta { get; set; } = string.Empty;
+        public DateTime? Fecha { get; set; }
+        public double? Valor { get; set; }
+        
+    }
+
+    public partial class Reporte
+    {
+        public DateTime? Fecha { get; set; }
+        public string? Cliente { get; set; }
+        public string? NumeroCuenta { get; set; }
+        public string? Tipo { get; set; }
+        public double SaldoInicial { get; set; }
+        public bool Estado { get; set; }
+        public double Movimiento { get; set; }
+        public double SaldoDisponible { get; set; }
+
+    }
+}
