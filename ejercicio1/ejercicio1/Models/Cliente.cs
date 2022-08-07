d904576b00a4699eaef2ed24a0696d845cbd3f6d
dannysan <dannysantqa@gmail.com>
2022-08-07 13:39:57 -0500

    Create Cliente.cs

---
+++ ejercicio1/ejercicio1/Models/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejercicio1.Models
+{
+    public partial class Cliente
+    {
+        public int ClieId { get; set; }
+        public int? PersId { get; set; }
+        public string? ClieContrasenia { get; set; }
+        public bool? ClieEstado { get; set; }
+    }
+
+    public partial class PersonaCliente
+    {
+        public int ClieId { get; set; }
+        public int? PersId { get; set; }
+        public string? ClieContrasenia { get; set; }
+        public bool? ClieEstado { get; set; }
+        public string? PersNombre { get; set; }
+        public string? PersGenero { get; set; }
+        public int? PersEdad { get; set; }
+        public string? PersIdentificacion { get; set; }
+        public string? PersDireccion { get; set; }
+        public string? PersTelefono { get; set; }
+    }
+}
